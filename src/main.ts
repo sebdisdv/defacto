7e03c9061b547c403a523b7a4fb93099462d0e8e
euberdeveloper <euberdeveloper@gmail.com>
2020-11-23 23:32:26 +0100

    started module aliases !!!!!!!!!!!!!!!!!!!!!!!

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'module-alias/register';
+
 import * as express from 'express';
 import * as http from 'http';
 import * as compression from 'compression';
@@ -6,7 +8,7 @@ import * as cors from 'cors';
 import * as morgan from 'morgan';
 import * as bodyParser from 'body-parser';
 
-import CONFIG from './config';
+import CONFIG from '@config';
 import router from './router';
 
 const app = express();
